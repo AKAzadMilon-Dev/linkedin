6902a58bf69d96e5b24cac88b75dd76ac0d75189
milon2267 <gdazad2267@gmail.com>
2022-07-02 15:41:28 +0600

    signin and signup page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./components/auth/Signin";
+import Signup from "./components/auth/Signup";
 import Home from "./components/pages/Home";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/signin" element={<Signin/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Col, Container, Row, Form, Button, Alert} from 'react-bootstrap';
 
-const Signin = () => {
+const Signup = () => {
   return (
     <div className='bgstyle'>
         <Container >
@@ -10,14 +10,24 @@ const Signin = () => {
                     <Form className='singinform'>
                         <img src='assets/images/logo-1.png'></img>
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Full Name" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm Password" />
                         </Form.Group>
 
                         <div className="d-grid gap-2">
@@ -29,7 +39,7 @@ const Signin = () => {
                     </Form>
                 </Col>
                 <Col lg={6}>
-                    <img src='assets/images/linkedin.png'></img>
+                
                 </Col>
             </Row>
         </Container>
@@ -37,4 +47,4 @@ const Signin = () => {
   )
 }
 
-export default Signin
+export default Signup
